5f06a9e4a0fb886c52f465ba73b06f58dc91aab8
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-03-30 23:35:42 +0530

    code for backend routes in next

---
+++ app/api/user/route.ts
@@ -0,0 +1,5 @@
+
+
+export function GET(){
+    return Response.json({name:"vidyadhar gowd j",email:"vidyadhariitkgp@gmail.com"})
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import axios from "axios";
 
 async function getUserDetails() {
-  await new Promise((r)=>setTimeout(r,5000))
+  await new Promise((r)=>setTimeout(r,2000))
   const response = await axios.get(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+    "http://localhost:3000/api/user"
   );
   return response.data;
 }
