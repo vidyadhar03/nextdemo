c45677fb9026b9246eab0cc36aefa009d84ca96a
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-03-31 15:47:48 +0530

    code to write sign up details to db dynamically

--- Components/SignUp.tsx
+++ Components/SignUp.tsx
@@ -1,13 +1,18 @@
 "use client";
-import { useState } from "react";
+import axios from "axios";
+import {  useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="border rounded-md shadow-md px-8 py-4">
-        <div className="p-1 pb-4 font-bold font-2xl text-center">Register to next demo</div>
+        <div className="p-1 pb-4 font-bold font-2xl text-center">
+          Register to next demo
+        </div>
         <div>
           <input
             placeholder="Email"
@@ -21,13 +26,28 @@ export default function SignUp() {
           <input
             placeholder="Password"
             onChange={(e) => {
-              setEmail(e.target.value);
+              setPassword(e.target.value);
             }}
             className="border rounded-md p-1"
           />
         </div>
-        <div className="flex justify-center"> 
-          <button className="bg-black text-white px-4 py-2 rounded-md mt-2 w-full">
+        <div className="flex justify-center">
+          <button
+            className="bg-black text-white px-4 py-2 rounded-md mt-2 w-full"
+            onClick={async () => {
+              console.log("started")
+
+              const response = await axios.post(
+                "http://localhost:3000/api/user",
+                {
+                  username:email,
+                  password,
+                }
+              );
+              console.log("success")
+              router.push("/");
+            }}
+          >
             Sign Up
           </button>
         </div>
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,18 @@
-
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
 
 export function GET(){
     return Response.json({name:"vidyadhar gowd j",email:"vidyadhariitkgp@gmail.com"})
+}
+
+export async function POST(req:NextRequest){
+    const body = await req.json();
+    await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })
+    return Response.json({message:"You are logged in !"})
 }
